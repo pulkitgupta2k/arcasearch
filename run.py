72c256c599723934e51ecd311518500b43f0265b
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-05-21 00:14:28 +0530

    changed range

--- run.py
+++ run.py
@@ -24,19 +24,19 @@ def driver():
         # for number_2 in range(999):
         #     for number_3 in range(9999):
                 # for number_4 in range(999):
-                    for number_5 in range(1,99999):
-                        flag = 0
-                        for number_6 in range(1,999):
-                            d_link = "{}-{}-{}-{}-{}_{}-{}.pdf".format(link, add_nulls(number_1, 3), add_nulls(number_2, 3), add_nulls(number_3, 4), add_nulls(number_4, 3), add_nulls(number_5, 5), add_nulls(number_6, 3))
-                            name = "{}-{}-{}-{}_{}-{}.pdf".format(str(number_1), str(number_2), str(number_3), str(number_4), str(number_5), str(number_6))
-                            status = download_file(d_link, name)
-                            print(d_link)
-                            if status == 404:
-                                if number_6 == 1:
-                                    flag = 1
-                                break
-                        if flag == 1:
-                            break
+        for number_5 in range(1,99999):
+            flag = 0
+            for number_6 in range(1,999):
+                d_link = "{}-{}-{}-{}-{}_{}-{}.pdf".format(link, add_nulls(number_1, 3), add_nulls(number_2, 3), add_nulls(number_3, 4), add_nulls(number_4, 3), add_nulls(number_5, 5), add_nulls(number_6, 3))
+                name = "{}-{}-{}-{}_{}-{}.pdf".format(str(number_1), str(number_2), str(number_3), str(number_4), str(number_5), str(number_6))
+                status = download_file(d_link, name)
+                if status == 404:
+                    if number_6 == 1:
+                        flag = 1
+                    break
+                print(d_link)
+            if flag == 1:
+                break
 
 
 if __name__ == "__main__":
